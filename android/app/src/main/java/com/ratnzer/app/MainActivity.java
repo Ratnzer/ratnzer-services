79935ade11aae8754a38682ecf70e05101b07247
Ratnzer <ratnzer@gmail.com>
2026-02-12 03:49:50 +0300

    Guard Firebase native plugin registration to prevent startup crash

--- android/app/src/main/java/com/ratnzer/app/MainActivity.java
+++ android/app/src/main/java/com/ratnzer/app/MainActivity.java
@@ -1,6 +1,23 @@
 package com.ratnzer.app;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import com.getcapacitor.BridgeActivity;
 
+import io.capawesome.capacitorjs.plugins.firebase.auth.FirebaseAuthenticationPlugin;
+
 public class MainActivity extends BridgeActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            this.registerPlugin(FirebaseAuthenticationPlugin.class);
+        } catch (Throwable error) {
+            Log.e(TAG, "FirebaseAuthentication plugin registration failed. App will continue without native social auth.", error);
+        }
+    }
 }
