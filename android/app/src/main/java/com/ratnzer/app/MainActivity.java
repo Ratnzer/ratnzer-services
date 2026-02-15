0bdbca1d7167f71f1ac1e0b83232603049afdb9e
Ratnzer <198170842+Ratnzer@users.noreply.github.com>
2026-02-15 16:39:22 -0500

    Fix: Add google-services.json and update Firebase SDK configuration to prevent app crash

--- android/app/src/main/java/com/ratnzer/app/MainActivity.java
+++ android/app/src/main/java/com/ratnzer/app/MainActivity.java
@@ -1,13 +1,5 @@
 package com.ratnzer.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Capacitor 6 handles plugin registration automatically.
-        // Manual registration of FirebaseAuthenticationPlugin is removed to prevent startup crashes.
-    }
-}
+public class MainActivity extends BridgeActivity {}
